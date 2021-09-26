69de15536dfa494a5ccec3ffc09dfe895cb67efb
CSIRamone <cidinei_inacio@hotmail.com>
2021-09-26 19:45:38 -0300

    Listing all releases

--- src/components/EntryList/index.js
+++ src/components/EntryList/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {getEntries} from '../../services/Entries';
 
-const EntryList = ({entries}) => {
+const EntryList = () => {
+  const [entries, setEntries] = useState([]);
+  useEffect(() => {
+    const loadEntries = async () => {
+      const data = await getEntries();
+      setEntries(data);
+    };
+
+    loadEntries();
+    console.log('EntryList :: useEffect');
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Últimos Lançamentos</Text>
