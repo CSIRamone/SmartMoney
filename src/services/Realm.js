2d2c018082a67af2cba2b793d4da5420f1987aaa
CSIRamone <cidinei_inacio@hotmail.com>
2021-10-05 02:10:07 -0300

    Ajust NewEntry - Categories part - 1

--- src/services/Realm.js
+++ src/services/Realm.js
@@ -1,11 +1,37 @@
 import Realm from 'realm';
 import CategorySchema from '../schemas/CategorySchema';
 import EntryShema from '../schemas/EntrySchema';
+import {getAllCategories} from '../services/Categories';
 
 export const getRealm = async () => {
   const realm = await Realm.open({
     schema: [CategorySchema, EntryShema],
-    schemaVersion: 1,
+    schemaVersion: 2,
   });
+  initDB(realm);
   return realm;
+};
+
+export const initDB = realm => {
+  const categoriesLength = realm.objects('Category').Length;
+  console.log(`Quantidade de categorias no DB :: iniDB : ${categoriesLength}`);
+
+  if (categoriesLength === 0) {
+    const categories = getAllCategories();
+
+    console.log('initDB :: initing db....');
+    try {
+      realm.write(() => {
+        categories.forEach(category => {
+          console.log(
+            `initDB :: creating category : ${JSON.stringify(category)}`,
+          );
+
+          realm.create('Category', category, true);
+        });
+      });
+    } catch (error) {}
+  } else {
+    console.log('initDB :: categories already existing...');
+  }
 };
