cff7cc55aba870c259c1e64ce98c018761f3c181
CSIRamone <cidinei_inacio@hotmail.com>
2021-11-11 02:04:39 -0300

    PieChart - part 1

--- src/components/EntrySummary/EntrySummaryChart/index.js
+++ src/components/EntrySummary/EntrySummaryChart/index.js
@@ -1,14 +1,43 @@
+import {random} from 'lodash';
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import {PieChart} from 'react-native-svg-charts';
+
 const EntrySummaryChart = () => {
+  const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80];
+
+  const randomColor = () =>
+    ('#' + ((Math.random() * 0xffffff) << 0).toString(16) + '000000').slice(
+      0,
+      7,
+    );
+
+  const chartData = data
+    .filter(value => value > 0)
+    .map((value, index) => ({
+      value,
+      svg: {
+        fill: randomColor(),
+        onPress: () => console.log('press', index),
+      },
+      key: `pie-${index}`,
+    }));
+
   return (
     <View>
-      <Text>EntrySummaryChart</Text>
+      <PieChart style={styles.chart} data={chartData} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  chart: {
+    height: 100,
+    width: 100,
+    marginRight: 10,
+  },
+});
 
 export default EntrySummaryChart;
--- src/components/EntrySummary/index.js
+++ src/components/EntrySummary/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import EntrySummaryChart from './EntrySummaryChart';
 import EntrySummaryList from './EntrySummaryList';
 import Container from '../Core/Container';
+import {View, StyleSheet} from 'react-native';
 
-const EntrySummary = ({onPressActionButton}) => {
+const EntrySummary = ({days = 7, onPressActionButton}) => {
   const entriesGrouped = [
     {key: '1', description: 'Alimentação: ', amount: 201},
     {key: '2', description: 'Combustível: ', amount: 12},
@@ -14,13 +15,22 @@ const EntrySummary = ({onPressActionButton}) => {
   return (
     <Container
       title="Categorias"
-      actionLabelText="Últimos 7 dias"
+      actionLabelText={`Últimos ${days} dias`}
       actionButtonText="Ver mais"
       onPressActionButton={onPressActionButton}>
-      <EntrySummaryChart />
-      <EntrySummaryList entriesGrouped={entriesGrouped} />
+      <View style={styles.inner}>
+        <EntrySummaryChart />
+        <EntrySummaryList entriesGrouped={entriesGrouped} />
+      </View>
     </Container>
   );
 };
+
+const styles = StyleSheet.create({
+  inner: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+  },
+});
 
 export default EntrySummary;
